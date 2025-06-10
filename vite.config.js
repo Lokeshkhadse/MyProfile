684a78d1936f60011bcbeeff068c891d05332db6
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-06-10 16:19:55 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  //  base: '/MyProfile/', // 👈 use your repo name here
-  plugins: [tailwindcss(),react()],
+  base: '/MyProfile/', // 👈 Required for GitHub Pages
+  plugins: [tailwindcss(), react()],
 })
